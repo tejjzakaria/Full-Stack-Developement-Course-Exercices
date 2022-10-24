de872a62bc1bc4adc38fb04716a05b8d27e0a5b6
tejjzakaria <zakaria.tejjani@gmail.com>
2022-10-24 17:03:05 +0100

    first commit 24/10/22

---
+++ public_html/script2.js
@@ -0,0 +1,28 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+function trait(){
+    /*traitement des  notes etudiants de classe*/
+    /*Déclaration des variables programme javascript*/
+    var nom =prompt("Donner le nom étudiant :");
+    var prenom =prompt("Donner le nom étudiant :");
+    var dn=parseInt(prompt("Donner la date de naissance"));
+    var note1 =parseFloat(prompt("Donner la note N°1 :"));
+    var note2 =parseFloat(prompt("Donner a note N°2 :"));
+    var note3 =parseFloat(prompt("Donner a note N°3 :"));
+    var note4 =parseFloat(prompt("Donner a note N°4 :"));
+     /*Affichage des données dans la console*/
+     console.log("Nom......... : "+nom);
+     console.log("Prenom Nom......... : "+prenom);
+     console.log("Date Naissance......... : "+dn);
+     console.log("la note 1......... : "+note1);
+     console.log("La note 2......... : "+note2);
+     console.log("la note3......... : "+note3);
+     console.log("la note4......... : "+note3);
+    
+}
+    
+  
+
---
+++ public_html/variables.js
@@ -0,0 +1,45 @@
+// declaration des variables
+// l'instruction "var" est une reservation de l'espace 
+
+var nom=prompt("Entrez le nom de la personne : ");
+var matricule=parseInt(prompt("Entrez le matricule : "));
+var note1=parseFloat(prompt("Entrez la note 1 : "));
+var note2=parseFloat(prompt("Entrez la note 2 : "));
+var note3=parseFloat(prompt("Entrez la note 3 : "));
+var note4=parseFloat(prompt("Entrez la note 4 : "));
+// calcul de la moyenne 
+var moy = (note1+note2+note3+note4)/4;
+// test conditionel sur la moyenne pour afficher la mention
+if(moy<10){
+    men1="Faible";
+}
+else if(moy>=10 && moy<14){
+    men2="Passable";
+}
+else if(moy>=12 && moy<14){
+    men3="Assez bien";
+}
+else if(moy>=14 && moy<16){
+    men4="Bien";
+}
+else if(moy>=16 && moy<18){
+    men5="Tres Bien";
+}
+else if(moy>=18 && moy<20){
+    men6="Excellent";
+}
+// affichage des variables
+
+console.log("le nom est : "+nom);
+console.log("le matricule est : "+matricule);
+console.log("la note 1 est : "+note1);
+console.log("la note 2 est : "+note2);
+console.log("la note 3 est : "+note3);
+console.log("la note 4 est : "+note4);
+console.log("la moyenne est : "+moy);
+console.log("la mention: "+men1);
+console.log("la mention: "+men2);
+console.log("la mention: "+men3);
+console.log("la mention: "+men4);
+console.log("la mention: "+men5);
+console.log("la mention: "+men6);
